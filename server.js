7d5b3f7856221c2cfa077f23bb01e6d5de99b498
mcampo0215 <matthewcampoverde@gmail.com>
2025-11-03 14:03:27 -0500

    Added users routes

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
 const app = express(); //calling this function sets up a server
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     console.log('Here');
-    res.send('Hello World');
+    res.render("index",
+        {user:"Matthew",});
 }); //this function will run when someone goes to the root folder
-
+app.get('/users', (req, res) => {
+    res.send('User List');
+});
+app.get('/users/new', (req, res) => {
+    res.send('New User Form');
+});
 app.listen(3030);
 
