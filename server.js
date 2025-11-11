6bec18c56959a0b4f4aa2a5b757603763b90e6dd
mcampo0215 <matthewcampoverde@gmail.com>
2025-11-11 01:12:29 -0500

    added posts route along with individual routes for /postslocation and /postsnew

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express');
-
+const userRouter = require('./routes/users');
 const app = express(); //calling this function sets up a server
 app.set('view engine', 'ejs');
+app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
     console.log('Here');
     res.render("index",
         {user:"Matthew",});
 }); //this function will run when someone goes to the root folder
-app.get('/users', (req, res) => {
-    res.send('User List');
-});
-app.get('/users/new', (req, res) => {
-    res.send('New User Form');
-});
+
 app.listen(3030);
 
